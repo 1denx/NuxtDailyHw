4392693c521bf6be551d484a58948caf12c96e12
Eden <1denchen.work@gmail.com>
2024-11-05 16:01:57 +0800

    Initialize project

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+  const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Page: index</h1>
+    <p>目前路由的路徑 - route.fullPath 為: {{ route.fullPath }}</p>
+  </div>
+</template>
+
+<style scoped></style>
