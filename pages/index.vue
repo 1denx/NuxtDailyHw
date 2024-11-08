ccbcff7b598bb8c95ecbb4349088264de484fdc2
Eden <1denchen.work@gmail.com>
2024-11-08 16:51:20 +0800

    [feat] 新增產品卡片

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,14 @@
-<script setup>
-const route = useRoute();
-</script>
+<script setup></script>
 
 <template>
   <div>
-    <h1>Page: index</h1>
-    <p>目前路由的路徑 - route.fullPath為 :{{ route.fullPath }}</p>
-    <button type="button" class="btn btn-primary">Primary</button>
-    <button type="button" class="btn btn-secondary">Secondary</button>
-    <button type="button" class="btn btn-success">Success</button>
-    <button type="button" class="btn btn-danger">Danger</button>
-    <button type="button" class="btn btn-warning">Warning</button>
-    <button type="button" class="btn btn-info">Info</button>
-    <button type="button" class="btn btn-light">Light</button>
-    <button type="button" class="btn btn-dark">Dark</button>
-
-    <button type="button" class="btn btn-link">Link</button>
+    <h1>產品</h1>
+    <ProductCard
+      imageSrc="https://images.unsplash.com/photo-1562438668-bcf0ca6578f0?q=80&w=300&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      altText="享受高級的住宿體驗，尊爵雙人房提供給您舒適寬敞的空間和精緻的裝潢"
+      title="尊爵雙人房"
+      price="NT$10000"
+      buttonText="馬上預約"
+    />
   </div>
 </template>
-
-<style lang="scss">
-$primary: blue;
-
-h1 {
-  color: $primary;
-  font-size: $fs-xl;
-}
-</style>
