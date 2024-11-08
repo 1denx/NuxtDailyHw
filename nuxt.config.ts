53af5eb8a53993b5a45949d336620b66cd4763d3
Eden <1denchen.work@gmail.com>
2024-11-08 12:57:21 +0800

    [feat] 新增 bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,18 @@
 export default defineNuxtConfig({
   // compatibilityDate 屬性 : 將 Nuxt3 的功能和行為鎖定在 2024-04-03 之前的版本，
   // 避免之後 Nuxt3 新版本的寫法調整會影響到目前專案的運作
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-08-16",
+  devtools: { enabled: true },
+  css: ["@/assets/stylesheets/all.scss"],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+            @import "@/assets/stylesheets/_variables.scss";
+          `,
+        },
+      },
+    },
+  },
+});
